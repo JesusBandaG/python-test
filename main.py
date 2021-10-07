33bda5338fdaad2f2ec3b3573459651788b2799c
JesusBandaG <banda.j94@gmail.com>
2021-10-06 23:14:21 -0500

    Extract function modified

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ import pandas as pd
 def extract_data():
   fileName = input("Ingrese la ruta del archivo en formato csv: ")
   print(f"Ha seleccionado el archivo '{fileName}'.")
-  df = pd.read_csv(fileName, sep=';')
+  dtypes = {
+    'fiscal_id': 'str',
+    'first_name': 'str',
+    'last_name': 'str',
+    'gender': 'str',
+    'fecha_nacimiento': 'str',
+    'fecha_vencimiento': 'str',
+    'deuda': 'int16',
+    'direccion': 'str',
+    'altura': 'int16',
+    'peso': 'int16',
+    'correo': 'str',
+    'estatus_contacto': 'str',
+    'prioridad': 'float',
+    'telefono': 'str'
+    }
+  parse_dates = ['fecha_nacimiento', 'fecha_vencimiento']
+  df = pd.read_csv(fileName, sep=';', dtype=dtypes, parse_dates=parse_dates)
+  df["prioridad"] = df['prioridad'].astype(pd.Int16Dtype())
   return df
 
 def transform_data(df):
